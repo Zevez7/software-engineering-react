d6816f4a279c091949eb413a487591f44bdcedeb
Zevez7 <datguy7@gmail.com>
2022-11-29 18:17:22 -0500

    fix update for tuit

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -44,22 +44,23 @@ export const findTuitsByUser = (uid) =>
   api.get(`${TUITS_API}/users/${uid}`).then((response) => response.data);
 /**
  * Find tuit by tuit id
- * @param  {string} tid tuit id
+ * @param {string} tid tuit id
  * return tuit found
  */
 export const findTuitById = (tid) =>
   axios.get(`${TUITS_API}/${tid}`).then((response) => response.data);
 /**
  * Delete tuit by user id
- * @param  {string} uid user id
+ * @param {string} uid user id
  * return delete status
  */
 export const deleteTuit = (uid) =>
   axios.delete(`${TUITS_API}/${uid}`).then((response) => response.data);
 /**
  * Update tuit by tuit id
- * @param  {string} tid tuit id
+ * @param {string} tid tuit id
+ * @param {tuit} tuit to be updated
  * return updated tuit
  */
-export const updateTuit = (tid) =>
-  axios.delete(`${TUITS_API}/${tid}`).then((response) => response.data);
+export const updateTuit = (tid, tuit) =>
+  axios.put(`${TUITS_API}/${tid}`, tuit).then((response) => response.data);
